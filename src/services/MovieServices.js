6581b583d31e36f59623708ddc06ae75563644dc
OtamNitram <martinmato90@gmail.com>
2021-03-22 09:26:19 -0300

    Style load more button

--- src/services/MovieServices.js
+++ src/services/MovieServices.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // *** Add you theMoviedb.org token here ***
-const AUTH_TOKEN = 'YOUR TOKEN HERE';
+const AUTH_TOKEN =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMTY3OGZiNDI4M2M1MTNhODAxYTFmODBjY2VjODllNiIsInN1YiI6IjYwNTZjZDJlNDU1N2EwMDA1MjEyZjU3ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PZg60jSNXGULeBIz692I93xjbyqZ_MOz0AyNJ7_lKWU';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 axios.defaults.headers.common['Authorization'] = `bearer ${AUTH_TOKEN}`;
--- src/views/MovieView.js
+++ src/views/MovieView.js
@@ -13,7 +13,6 @@ function MovieView() {
     error,
     fetchNextPage,
     hasNextPage,
-    isFetching,
     isFetchingNextPage,
     status,
   } = useInfiniteQuery('projects', getPopularMovies, {
@@ -44,20 +43,44 @@ function MovieView() {
             ))}
           </React.Fragment>
         ))}
-        <div>
-          <button
-            onClick={() => fetchNextPage()}
-            disabled={!hasNextPage || isFetchingNextPage}>
-            {isFetchingNextPage
-              ? 'Loading more...'
-              : hasNextPage
-              ? 'Load More'
-              : 'Nothing more to load'}
-          </button>
-        </div>
-        <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
-        <ReactQueryDevtools initialIsOpen />
       </div>
+      <div className="p-4 ">
+        <button
+          className="w-full h-12 px-6 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800"
+          onClick={() => fetchNextPage()}
+          disabled={!hasNextPage || isFetchingNextPage}>
+          {isFetchingNextPage ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="mx-auto animate-spin w-5 h-5 fill-current ">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+              />
+            </svg>
+          ) : hasNextPage ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="mx-auto animate-bounce w-5 h-5 fill-current ">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
+                clipRule="evenodd"
+              />
+            </svg>
+          ) : (
+            'Nothing more to load'
+          )}
+        </button>
+      </div>
+      <ReactQueryDevtools initialIsOpen />
     </div>
   );
 
